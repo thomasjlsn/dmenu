867a895b4255981de00420aa597fdf609d78568c
Thomas Ellison <thomasjlsn@gmail.com>
2020-08-25 11:23:11 -0700

    imported my fork of dmenu

---
+++ colors.tj.h
@@ -0,0 +1,8 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#000000" },
+	[SchemeSel] = { "#eeeeee", "#81A2BE" },
+	[SchemeSelHighlight] = { "#CC6666", "#81A2BE" },
+	[SchemeNormHighlight] = { "#CC6666", "#000000" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
